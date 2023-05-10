0b1c44ed0e14a66cd98bee95e65f5e306bff79a4
2130056 <a2130056@2130056ui-MacBookPro.local>
2023-05-10 10:32:00 +0900

    change components

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -15,7 +15,7 @@ export const Text = <E extends React.ElementType>({
   const Component = as || 'div';
   let classString = "text";
   if(['h1','h2','h3','h4','h5','h6'].indexOf(as as string) < 0) {
-    classString = ["text", `text--${size}`,].join(" ");
+    classString = ["text", `text--${size}`].join(" ");
   }
 
   return (
